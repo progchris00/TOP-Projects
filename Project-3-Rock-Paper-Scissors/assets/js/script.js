160400e74ba2e988a22952feb679865f5ef54fc1
progchris00 <prog.christian07@gmail.com>
2024-06-07 19:09:17 +0800

    update: apply prettier formatting on script.js and remove prompting user for choice

--- Project-3-Rock-Paper-Scissors/assets/js/script.js
+++ Project-3-Rock-Paper-Scissors/assets/js/script.js
@@ -3,51 +3,50 @@ let humanScore = 0;
 let computerScore = 0;
 
 const getComputerChoice = () => {
-    const choices = ["rock", "paper", "scissors"];
-    const choice = Math.floor(Math.random() * 3);
-    return choices[choice];
-}
-
-const getHumanChoice = () => {
-    return prompt();
-} 
+  const choices = ["rock", "paper", "scissors"];
+  const choice = Math.floor(Math.random() * 3);
+  return choices[choice];
+};
 
 const playRound = (humanChoice, computerChoice) => {
-    const beatsBy = {
-        "scissors":"rock", 
-        "rock":"paper",
-        "paper":"scissors"
-    };
+  const beatsBy = {
+    scissors: "rock",
+    rock: "paper",
+    paper: "scissors",
+  };
 
-    if (beatsBy[humanChoice] === computerChoice) {
-        console.log(`You lose!, ${computerChoice} beats ${humanChoice}`);
-        computerScore += 1;
-    } else if (beatsBy[computerChoice] === humanChoice && humanChoice !== computerChoice){
-        console.log("You won!");
-        humanScore += 1;
-    } else {
-        console.log("Draw!");
-    }
+  if (beatsBy[humanChoice] === computerChoice) {
+    console.log(`You lose!, ${computerChoice} beats ${humanChoice}`);
+    computerScore += 1;
+  } else if (
+    beatsBy[computerChoice] === humanChoice &&
+    humanChoice !== computerChoice
+  ) {
+    console.log("You won!");
+    humanScore += 1;
+  } else {
+    console.log("Draw!");
+  }
 };
 
 const announceWinner = () => {
-    if (humanScore > computerScore) {
-        console.log("You won!");
-    } else if (humanScore < computerScore) {
-        console.log(`You lose! ${humanScore}-${computerScore}`);
-    } else {
-        console.log("Draw!");
-    };
-}
+  if (humanScore > computerScore) {
+    console.log("You won!");
+  } else if (humanScore < computerScore) {
+    console.log(`You lose! ${humanScore}-${computerScore}`);
+  } else {
+    console.log("Draw!");
+  }
+};
 
 const playGame = () => {
-    for (let index = 0; index < rounds; index++) {
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-    };
+  for (let index = 0; index < rounds; index++) {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+  }
 
-    announceWinner();
-}
+  announceWinner();
+};
 
-playGame()
+playGame();
