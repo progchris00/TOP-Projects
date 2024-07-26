1f664e02bc9e503aa21d9b16a54d7018c05295d0
progchris00 <prog.christian07@gmail.com>
2024-07-26 18:35:25 +0800

    feat: add color picker and clear button

--- Project-4-Etch-A-Sketch/script.js
+++ Project-4-Etch-A-Sketch/script.js
@@ -1,7 +1,10 @@
 const container = document.querySelector(".container");
+const picker = document.querySelector(".picker");
+const clearButton = document.querySelector(".clear-button");
 
-const rowTotalCount = 16;
-const columnTotalCount = 16;
+const rowTotalCount = 32;
+const columnTotalCount = 32;
+
 let pixelCount = 0;
 let rowCount = 0;
 
@@ -21,22 +24,30 @@ for (let row = 1; row <= rowTotalCount; row++) {
 }
 
 let isDrawing = false;
+let currentColor;
 
 const allPixels = document.querySelectorAll(".pixel");
 
 allPixels.forEach((pixel) => {
   pixel.addEventListener("mousedown", () => {
+    currentColor = picker.value;
     isDrawing = true;
-    pixel.style.background = "black";
+    pixel.style.background = currentColor;
   });
 
   pixel.addEventListener("mouseover", () => {
     if (isDrawing) {
-      pixel.style.background = "black";
+      pixel.style.background = currentColor;
     }
   });
 
   pixel.addEventListener("mouseup", () => {
     isDrawing = false;
+  });
+});
+
+clearButton.addEventListener("click", () => {
+  allPixels.forEach((pixel) => {
+    pixel.style.background = "white";
   });
 });
