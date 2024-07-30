d50e312072a528b68ad9dab723ed2b641f07faf0
progchris00 <prog.christian07@gmail.com>
2024-07-30 14:59:37 +0800

    feat: implement erasing

--- Project-4-Etch-A-Sketch/script.js
+++ Project-4-Etch-A-Sketch/script.js
@@ -4,12 +4,16 @@ const clearButton = document.querySelector(".clearing-tool");
 const sketchingArea = document.querySelector(".sketching-area");
 const gridSizeSlider = document.querySelector(".grid-size-slider");
 const gridCurrentSize = document.querySelector(".grid-current-size");
+const eraserTool = document.querySelector(".erasing-tool");
 
 let columnCount = 16;
 let rowCount = 16;
 let gridSize = columnCount * rowCount;
 let allPixels;
 let isDrawing = false;
+let mouseDown = false;
+let isErasing = false;
+let eraserMode = false;
 let currentColor;
 
 const loadGrid = () => {
@@ -25,31 +29,51 @@ const loadGrid = () => {
   attachEvent();
 };
 
-gridSizeSlider.addEventListener("mousemove", () => {
-  columnCount = gridSizeSlider.value;
-  rowCount = gridSizeSlider.value;
-  gridSize = columnCount * rowCount;
-  sketchingArea.innerHTML = "";
+gridSizeSlider.addEventListener("mousedown", () => {
+  mouseDown = true;
 
-  loadGrid();
+  gridSizeSlider.addEventListener("mousemove", () => {
+    if (mouseDown) {
+      gridCurrentSize.textContent = `Grid count: ${columnCount} x ${rowCount}`;
+      columnCount = gridSizeSlider.value;
+      rowCount = gridSizeSlider.value;
+      gridSize = columnCount * rowCount;
+      sketchingArea.innerHTML = "";
+      loadGrid();
+    }
+
+    gridSizeSlider.addEventListener("mouseup", () => {
+      mouseDown = false;
+    });
+  });
 });
 
 const attachEvent = () => {
   allPixels.forEach((pixel) => {
     pixel.addEventListener("mousedown", () => {
-      currentColor = picker.value;
-      isDrawing = true;
-      pixel.style.background = currentColor;
+      if (eraserMode) {
+        pixel.style.background = "white";
+        isErasing = true;
+      } else {
+        currentColor = picker.value;
+        isDrawing = true;
+        pixel.style.background = currentColor;
+      }
     });
 
     pixel.addEventListener("mouseover", () => {
       if (isDrawing) {
         pixel.style.background = currentColor;
       }
+
+      if (isErasing) {
+        pixel.style.background = "white";
+      }
     });
 
     pixel.addEventListener("mouseup", () => {
       isDrawing = false;
+      isErasing = false;
     });
 
     pixel.addEventListener("drag", () => {
@@ -62,6 +86,11 @@ clearButton.addEventListener("click", () => {
   allPixels.forEach((pixel) => {
     pixel.style.background = "white";
   });
+});
+
+eraserTool.addEventListener("click", () => {
+  eraserMode = true;
+  eraserTool.classList.toggle("active");
 });
 
 loadGrid();
