be1289ea3286129fe05d8176e9a9dfed410f7c7b
progchris00 <prog.christian07@gmail.com>
2024-10-19 20:33:20 +0800

    feat: create calculator class

--- Project-5-Calculator/main.js
+++ Project-5-Calculator/main.js
@@ -1,12 +1,34 @@
-const buttons = document.querySelectorAll("button");
-const input = document.getElementById("input");
+// const buttons = document.querySelectorAll("button");
+// const input = document.getElementById("input");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    if (input.textContent == 0) {
-      input.textContent = button.textContent;
-    } else {
-      input.textContent += button.textContent;
-    }
-  });
-});
+// buttons.forEach((button) => {
+//   button.addEventListener("click", () => {
+//     if (input.textContent == 0) {
+//       input.textContent = button.textContent;
+//     } else {
+//       input.textContent += button.textContent;
+//     }
+//   });
+// });
+
+class Calculator {
+  constructor() {
+    this.inputField = document.querySelector(".input");
+    this.buttons = document.querySelectorAll("button");
+  }
+
+  applyButtonListener() {
+    this.buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        if (this.inputField.textContent == 0) {
+          this.inputField.textContent = button.textContent;
+        } else {
+          this.inputField.textContent += button.textContent;
+        }
+      });
+    });
+  }
+}
+
+const TOPCalculator = new Calculator();
+TOPCalculator.applyButtonListener();
