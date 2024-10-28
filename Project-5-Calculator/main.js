297f00a721c9cdffddf1db10b2b33e2de5abe8d3
progchris00 <prog.christian07@gmail.com>
2024-10-28 09:14:09 +0800

    feat: working on simple expressions
    Not working on multiple evaluation

--- Project-5-Calculator/main.js
+++ Project-5-Calculator/main.js
@@ -16,39 +16,63 @@ class Calculator {
     this.buttons.forEach((button) => {
       button.addEventListener("click", () => {
         if (Calculator.NUMERICS.includes(button.value)) {
-          if (this.inputField.textContent == 0) {
-            this.inputField.textContent = button.value;
-          } else {
-            this.inputField.textContent += button.value;
-          }
-
-          this.temporaryNumberStorage.push(button.value);
+          this.handleNumerics(button);
+        } else if (Calculator.OPERATIONS.includes(button.value)) {
+          this.handleOperations(button);
+        } else if (button.value == "=") {
+          this.handleEqual();
         }
 
-        if (Calculator.OPERATIONS.includes(button.value)) {
-          this.operator = button.value;
-          this.inputField.textContent += this.operator;
-
-          if (this.firstNumber == null) {
-            this.firstNumber = +this.temporaryNumberStorage.join("");
-          } else {
-            this.secondNumber = +this.temporaryNumberStorage.join("");
-            this.operate();
-          }
-
-          this.temporaryNumberStorage = [];
-        }
-
-        if (button.value == "=") {
-          this.secondNumber = +this.temporaryNumberStorage.join("");
-          this.operate();
-        }
-        console.log(this.temporaryNumberStorage);
+        console.log(TOPCalculator);
       });
     });
   }
 
+  handleNumerics(button) {
+    // Replace input field with the button value the first time and after an expression. Append otherwise.
+
+    if (this.inputField.textContent == 0) {
+      this.inputField.textContent = button.value;
+    } else if (this.result) {
+      this.inputField.textContent = button.value;
+      this.result = null;
+    } else {
+      this.inputField.textContent += button.value;
+    }
+
+    // Combine multiple digit numbers after first number
+    if (this.firstNumber) {
+      if (this.temporaryNumberStorage == 0) {
+        this.inputField.textContent = button.value;
+      }
+    }
+
+    this.temporaryNumberStorage.push(button.value);
+  }
+
+  handleOperations(button) {
+    // Set operation when clicked. Evaluate expression for the first time and also when one is evaluated.
+
+    this.operator = button.value;
+
+    if (this.result) {
+      this.firstNumber = this.result;
+      this.result = null;
+    } else {
+      this.firstNumber = +this.temporaryNumberStorage.join("");
+      this.temporaryNumberStorage = [];
+    }
+  }
+
+  handleEqual() {
+    if (this.firstNumber && this.temporaryNumberStorage) {
+      this.operate();
+    }
+    this.temporaryNumberStorage = [];
+  }
+
   operate() {
+    this.secondNumber = +this.temporaryNumberStorage.join("");
     switch (this.operator) {
       case "+":
         this.add();
