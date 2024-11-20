4af9b415067ceec3d1d0fc87f7345b8b2476c2bf
progchris00 <prog.christian07@gmail.com>
2024-11-20 17:38:09 +0800

    feat: add starting js logic

--- Project-8-Library/main.js
+++ Project-8-Library/main.js
@@ -1,27 +1,49 @@
 const myLibrary = [];
+const openModalButton = document.getElementById("open-modal-button");
+const closeModal = document.getElementById("close-modal-button");
+const addNewBookModal = document.getElementById("add-new-book-modal");
+
+const submitNewBookButton = document.getElementById("submit-new-book-button");
 
 function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  const book = new Book(title, author, pages, read);
-  myLibrary.push(book);
+    const book = new Book(title, author, pages, read);
+    myLibrary.push(book);
 }
 
 function displayBooks() {
-  myLibrary.forEach((book) => {
-    const bookContainer = document.querySelector(".book-container");
-    bookContainer.innerHTML += `
+    myLibrary.forEach((book) => {
+        const bookContainer = document.querySelector(".books-container");
+        bookContainer.innerHTML += `
         <li>Book Title: ${book.title} </li>
         <li>Author: ${book.author} </li>
         <li>Pages: ${book.pages} </li>
         <li> ${book.read} </li
   `;
-  });
+    });
 }
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+openModalButton.addEventListener("click", () => {
+    addNewBookModal.showModal();
+})
+closeModal.addEventListener("click", () => {
+    addNewBookModal.close();
+})
+
+submitNewBookButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    const bookTitle = document.getElementById("title").value;
+    const bookAuthor = document.getElementById("author").value;
+    const bookPages = document.getElementById("pages").value;
+    let bookRead = document.getElementById("read").value;
+    addBookToLibrary(bookTitle, bookAuthor, bookPages, bookRead);
+    addNewBookModal.close();
+    displayBooks();
+})
+
