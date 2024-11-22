96cabe814af7ed67b78e67bd21940d622588152c
progchris00 <prog.christian07@gmail.com>
2024-11-22 19:53:42 +0800

    feat: toggle working

--- Project-8-Library/main.js
+++ Project-8-Library/main.js
@@ -17,16 +17,27 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
+function toggleStatus(bookID) {
+    const button = document.getElementById(bookID);
+    let bookStatus = button.getAttribute("data-status");
+    bookStatus = bookStatus === "true" ? "false" : "true";
+    button.setAttribute("data-status", bookStatus);
+    button.textContent = bookStatus === "true" ? "Read" : "Not Read";
+    myLibrary[bookID].read = bookStatus === "true";
+}
+
 function displayBooks() {
     const bookContainer = document.querySelector(".books-container");
     bookContainer.innerHTML = "";
     let bookID = 0;
     myLibrary.forEach((book) => {
+        let readStatus = book.read === true ? "Read" : "Not Read";
         bookContainer.innerHTML += `
         <div data-attribute=${bookID}>
             <p>Book Title: ${book.title} </p>
             <p>Author: ${book.author} </p>
             <p>Pages: ${book.pages} </p>
+            <button data-status="${book.read}" onclick="toggleStatus(${bookID})" id="${bookID}">${readStatus}</button>
             <p> ${book.read} </p>
             <button class="delete-button">Remove book</button>
         </div>
