aea4e33c04efec7270ac6a9ec700dfb4412ceab6
progchris00 <prog.christian07@gmail.com>
2024-11-27 18:49:44 +0800

    feat: remove unused code

--- Project-9-Tic-Tac-Toe/main.js
+++ Project-9-Tic-Tac-Toe/main.js
@@ -1,19 +1,3 @@
-// function checkGameStatus() {
-//     const winningCombination = ["012", "345", "678", "036", "147", "258", "048", "246"];
-//     const turns = [];
-//     winningCombination.forEach((combination) => {
-//         playerOneTurns.forEach((turn) => {
-//             if (combination.includes(turn.toString())) {
-//                 turns.push(combination);
-//             }
-//         })
-//         let turnCount = turns.filter(turn => turn === combination).length;
-//         if (turnCount === 3) {
-//             console.log("You won!");
-//         }
-//     })
-// }
-
 function createPlayer(name) {
     const playerName = name;
     const playerTurns = [];
