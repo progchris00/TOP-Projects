5119b2e483918ce1e4a0fd92647aa85bbba5f0e6
progchris00 <prog.christian07@gmail.com>
2024-11-27 18:49:30 +0800

    feat: create a temporary module for gameBoard

--- Project-9-Tic-Tac-Toe/main.js
+++ Project-9-Tic-Tac-Toe/main.js
@@ -1,18 +1,18 @@
-function checkGameStatus() {
-    const winningCombination = ["012", "345", "678", "036", "147", "258", "048", "246"];
-    const turns = [];
-    winningCombination.forEach((combination) => {
-        playerOneTurns.forEach((turn) => {
-            if (combination.includes(turn.toString())) {
-                turns.push(combination);
-            }
-        })
-        let turnCount = turns.filter(turn => turn === combination).length;
-        if (turnCount === 3) {
-            console.log("You won!");
-        }
-    })
-}
+// function checkGameStatus() {
+//     const winningCombination = ["012", "345", "678", "036", "147", "258", "048", "246"];
+//     const turns = [];
+//     winningCombination.forEach((combination) => {
+//         playerOneTurns.forEach((turn) => {
+//             if (combination.includes(turn.toString())) {
+//                 turns.push(combination);
+//             }
+//         })
+//         let turnCount = turns.filter(turn => turn === combination).length;
+//         if (turnCount === 3) {
+//             console.log("You won!");
+//         }
+//     })
+// }
 
 function createPlayer(name) {
     const playerName = name;
@@ -25,3 +25,38 @@ function createPlayer(name) {
 
     return {setTurns, getTurns};
 }
+
+(function () {
+    const winningCombination = ["012", "345", "678", "036", "147", "258", "048", "246"];
+    const isGameOver = false;
+
+    const playerOne = createPlayer("Player One")
+    const playerTwo = createPlayer("Player Two")
+
+    const takeTurns = () => {
+        while (isGameOver === false) {
+            playerOne.setTurns();
+            checkGameStatus(playerOne.getTurns());
+
+            playerTwo.setTurns();
+            checkGameStatus(playerTwo.getTurns());
+        }
+    }
+
+    const checkGameStatus = (playerTurns) => {
+        const turns = [];
+
+        winningCombination.forEach((combination) => {
+            playerTurns.forEach((turn) => {
+                if (combination.includes(turn.toString())) {
+                    turns.push(combination);
+                }
+            })
+
+            let turnCount = turns.filter(turn => turn === combination).length;
+            if (turnCount === 3) {
+                console.log("Game Over!");
+            }
+        })
+    }
+})();
