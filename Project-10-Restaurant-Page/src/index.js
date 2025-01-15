d15c0cd573a9b2f15d80ff641123585dc7a9568e
progchris00 <prog.christian07@gmail.com>
2025-01-15 10:57:53 +0800

    feat: add logic for loading tabs

--- Project-10-Restaurant-Page/src/index.js
+++ Project-10-Restaurant-Page/src/index.js
@@ -1,3 +1,31 @@
 import { loadHomepage } from "./homepage";
+import { loadMenupage } from "./menupage";
+import { loadAboutpage } from "./aboutpage";
 
-loadHomepage();
+document
+  .getElementById("home-btn")
+  .addEventListener("click", () => loadPage("home"));
+
+document
+  .getElementById("menu-btn")
+  .addEventListener("click", () => loadPage("menu"));
+
+document
+  .getElementById("about-btn")
+  .addEventListener("click", () => loadPage("about"));
+
+function loadPage(page) {
+  if (page == "home") {
+    loadHomepage();
+  } else if (page == "about") {
+    loadAboutpage();
+  } else if (page == "menu") {
+    loadMenupage();
+  }
+}
+
+function initialPageLoad() {
+  loadHomepage();
+}
+
+initialPageLoad();
