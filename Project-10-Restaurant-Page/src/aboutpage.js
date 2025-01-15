ed6909a0013c2481ed557f921cfb71573b36170d
progchris00 <prog.christian07@gmail.com>
2025-01-15 10:35:17 +0800

    feat: create about page

---
+++ Project-10-Restaurant-Page/src/aboutpage.js
@@ -0,0 +1,14 @@
+export function loadAboutpage() {
+  const content = document.getElementById("content");
+  content.innerHTML = `
+    <div class="wrapper">
+      <div class="text-container">
+        <h1>Menu page</h1>
+        <p>This is the menu page</p>
+      </div>
+      <div class="image-container">
+        <img src="" alt="Delicious burger" />
+      </div>
+    </div>
+  `;
+}
